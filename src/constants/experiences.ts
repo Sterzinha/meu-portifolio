1a69876b833f205f9bd210bc17901cbfedb0148d
Ster Roque <79429966+SterRoque@users.noreply.github.com>
2026-07-01 12:30:54 -0300

    feat: update work experiences

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -3,16 +3,24 @@ export const experiences = [
       name: 'Leader Media',
       role: 'Desenvolvedora Front-End',
       start: '01/01/2024',
-      end: 'atual',
+      end: '30/04/2025',
       description:
-         'Trabalho prestando serviços freelancers no RedePet, mobile e web.',
+         'Atuei no desenvolvimento do RedePet, um app de rastreamento de pets. No mobile, construí telas e funcionalidades com React Native, TypeScript e Styled Components. Na web, desenvolvi a landing page de download com AstroJS e Tailwind, cuidando da responsividade e da experiência do usuário.',
    },
    {
       name: 'VarejoChip',
       role: 'Desenvolvedora Front-End',
       start: '10/07/2024',
-      end: 'atual',
+      end: '30/04/2025',
       description:
-         'Trabalho prestando serviços freelancers para o site da VarejoChip.',
+         'Fui responsável pelo desenvolvimento e manutenção do site da VarejoChip, criando componentes e novas seções com React, TypeScript e Tailwind. Atuei na construção de interfaces responsivas, correção de bugs e melhorias de performance.',
+   },
+   {
+      name: 'Metrópole GG',
+      role: 'Desenvolvedora Fullstack',
+      start: '06/05/2025',
+      end: '11/04/2026',
+      description:
+         'Atuei no desenvolvimento FiveM, criando e mantendo scripts e resources em TypeScript e Lua. Desenvolvi novas interfaces de jogo com React, Tailwind e Styled Components e implementei endpoints na API em .NET e C# para dar suporte a novas funcionalidades, trabalhando de ponta a ponta entre front-end, back-end e integração com o jogo.',
    },
 ];
